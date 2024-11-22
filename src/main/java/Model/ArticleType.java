3bce7aeadb64d96898b438e28b560f910cd939b9
HP <juanes52880@gmail.com>
2024-11-21 22:17:40 -0500

    Commit hecho por Juan Esteban Quintero, Agregue package Model

--- src/main/java/Model/ArticleType.java
+++
@@ -1,9 +0,0 @@
-
-package Model;
-
-public enum ArticleType {  // Usar enumeracion para usar valores definidos co,o los siguientes 
-    BOOK, //Libro
-    MAGAZINE, //Revista 
-    VIDEO_MEDIA, //Video
-}
-
---
+++ src/main/java/Model/VideoMediaModel.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class VideoMediaModel  {
+    private String hours; // Duración 
+
+public VideoMediaModel(String hours){
+    this.hours= hours;
+}
+
+    public String gethours() {
+        return hours;
+    }
+
+    public void sethours(String duration) {
+        this.hours = duration;
+    }
+}
