af968f08680f80e4fb6c7f936eb10d6079d9a96d
arqua <arquantos66@hotmail.com>
2024-11-21 22:05:53 -0500

    se agregaron el paquete view para book, videomedia, magazine

---
+++ src/main/java/view/MagazineView.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author arqua
+ */
+public class MagazineView {
+    
+    public static void updateMagazine() {
+        
+    } 
+    
+    public static void insertMagazine() {
+        
+    } 
+    
+    public static void removeMagazine() {
+        
+    } 
+    
+    public static void listMagazine() {
+        
+    } 
+    
+    public static void listMagazines() {
+        
+    }
+    
+}
---
+++ src/main/java/view/VideoMediaView.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author arqua
+ */
+public class VideoMediaView {
+    
+    public static void updateVideoMedia() {
+        
+    } 
+    
+    public static void insertVideoMedia() {
+        
+    } 
+    
+    public static void removeVideoMedia() {
+        
+    } 
+    
+    public static void listVideoMedia() {
+        
+    } 
+    
+    public static void listVideoMedias() {
+        
+    }
+    
+}
