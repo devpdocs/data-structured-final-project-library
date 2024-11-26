1d622b89f75bf9f67ac9b7baa5bd79e604201ee4
JuanEQuintero <juanes52880@gmail.com>
2024-11-25 20:36:03 -0500

    Update VideoMediaModel.java
    Se realizan cambios en la clase VideoMediaModel

--- src/main/java/model/VideoMediaModel.java
+++ src/main/java/model/VideoMediaModel.java
@@ -1,9 +1,22 @@
-package model;
+package Model;
 
-public class VideoMediaModel  {
+import Interfaces.MaterialType;
+import java.time.LocalDate;
+
+public class VideoMediaModel extends InventoryModel{
     private String hours; // Duración 
 
-public VideoMediaModel(String hours){
+public VideoMediaModel(String hours, int id,
+String title,
+String author,
+String editorial,
+String publishedYear,
+MaterialType materialType,
+boolean availableArticle,
+LocalDate createdAt,
+LocalDate updatedAt,
+int ncopy){
+    super(id, title, author, editorial, publishedYear, MaterialType.VIDEO_MEDIA, availableArticle, createdAt, updatedAt, ncopy);
     this.hours= hours;
 }
 
