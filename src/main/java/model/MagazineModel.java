7630663d100312c683b77f16231acbc88ad80123
JuanEQuintero <juanes52880@gmail.com>
2024-11-25 20:34:03 -0500

    Update MagazineModel.java
    Se realiza cambios en la clase MagazineModel

--- src/main/java/model/MagazineModel.java
+++ src/main/java/model/MagazineModel.java
@@ -1,10 +1,24 @@
 package model;
 
-public class MagazineModel {
+import interfaces.MaterialType;
+import java.time.LocalDate;
+
+public class MagazineModel extends InventoryModel {
     private String branch; // Tipo de revista
     private String gender; // Género de la revista
 
-    public MagazineModel(String branch, String gender) {
+    public MagazineModel(String branch, String gender,int nPages,
+    int id,
+    String title,
+    String author,
+    String editorial,
+    String publishedYear,
+    MaterialType materialType,
+    boolean availableArticle,
+    LocalDate createdAt,
+    LocalDate updatedAt,
+    int ncopy) {
+        super(id, title, author, editorial, publishedYear, MaterialType.MAGAZINE, availableArticle, createdAt, updatedAt, ncopy);
         this.branch = branch;
         this.gender = gender;
     }
@@ -25,4 +39,3 @@ public class MagazineModel {
         this.gender = gender;
     }
 }
-
